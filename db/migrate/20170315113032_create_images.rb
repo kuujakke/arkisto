137daccba3f655c01240a40d75ba8e121cb8c236
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-15 13:34:38 +0200

    Luodaan Image model

---
+++ db/migrate/20170315113032_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :images do |t|
+      t.string :file
+      t.string :source
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
