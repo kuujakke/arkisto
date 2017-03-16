9015ae640454088bde7a6c81bfd71ef780657af5
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-16 20:21:46 +0200

    Validoi parametrit

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with valid parameters" do
+    let(:image) { Image.create file: "191231239.jpg", source: "https://www.asd.com/foo/bar" }
+
+    it "should be saved" do
+      expect(image).to be_valid
+    end
+
+    it "should have supported filetype" do
+    end
+  end
+  describe "with invalid parameters" do
+    let(:image) { Image.create file: "asd.jiipeegee", source: "teh-inter.nets" }
+
+    it "should not be saved" do
+      expect(image).not_to be_valid
+    end
+  end
 end
