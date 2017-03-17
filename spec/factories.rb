4399773b35c4856e56226387e0b71df86c9b479c
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-17 20:13:07 +0200

    Lisätty sovelluksen ominaisuuksien määrittely.

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,12 @@ FactoryGirl.define do
     file "1489500739386.jpg"
     source_url "https://www.asd.net/foo/bar"
   end
+  factory :image_png, class: Image do
+    file "1489500739386.png"
+    source_url "https://www.asd.net/foo/bar"
+  end
+  factory :image_webp, class: Image do
+    file "1489500739386.webp"
+    source_url "https://www.asd.net/foo/bar"
+  end
 end
