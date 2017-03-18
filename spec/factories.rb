650378c497cb672d5a0bd15ea9e578cd8e3bd0e4
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-18 22:45:44 +0200

    Adds some valid extension examples

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,12 @@ FactoryGirl.define do
     file "1489500739386.jpg"
     source_url "https://www.asd.net/foo/bar"
   end
+  factory :picture_png, class: Picture do
+    file "1489500739386.png"
+    source_url "https://www.asd.net/foo/bar"
+  end
+  factory :picture_webp, class: Picture do
+    file "1489500739386.webp"
+    source_url "https://www.asd.net/foo/bar"
+  end
 end
