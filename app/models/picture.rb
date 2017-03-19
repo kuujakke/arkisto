eee24f0d9664377ff2942211eadfe4c7ef2be10a
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-19 23:04:18 +0200

    Fix typo in :path

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ApplicationRecord
   has_attached_file :file,
                     :styles => { :thumb => "200x200" },
-                    :path => "rails_root/app/assets/images/:basename.:extension"
+                    :path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :file, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/webp"]
   validates :source_url,
             format: {
