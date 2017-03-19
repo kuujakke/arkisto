502f8ac08656ab98659a0bc32b9d8f055029123b
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-19 17:13:24 +0200

    Add user navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   get 'pictures/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
 end
