e2b56afa09ca849a09681f734cb23fb953f09e63
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-26 16:13:48 +0300

    Make test for multiple pictures

--- spec/views/pictures/index.html.haml_spec.rb
+++ spec/views/pictures/index.html.haml_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "pictures/index.html.haml", type: :view do
   describe "with many pictures" do
+    before :each do
+      FactoryGirl.create :picture_jpeg
+      FactoryGirl.create :picture_png
+      FactoryGirl.create :picture_webp
+      FactoryGirl.create :picture_gif
+    end
     it "should display many pictures" do
-
+      visit pictures_path
+      expect(page).to have_css('img', :count => 4)
+      save_and_open_page
     end
   end
 end
