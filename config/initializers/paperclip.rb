6cc383659f1b00da47abf55c5398407ddc4a863f
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-29 00:35:25 +0300

    Add monkey patch for skipping Paperclips MediaTypeSpoofDetection

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,21 @@
-Paperclip.options[:content_type_mappings] = {
-    jpg: 'image/jpg',
-    jpeg: 'image/jpeg',
-    gif: 'image/gif',
-    webp: 'image/webp',
-    png: 'image/png'
-}
+module Paperclip
+  # do not require any validations
+  REQUIRED_VALIDATORS = []
+
+  # do not complain when missing validations
+  class Attachment
+    def missing_required_validator?
+      false
+    end
+  end
+
+  # skip media type spoof detection
+  module Validators
+    class MediaTypeSpoofDetectionValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        true
+      end
+    end
+  end
+
+end
