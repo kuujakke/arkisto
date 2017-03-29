94982fe0c85ae99f312a58e6d5d0898dba73d12a
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-03-29 13:16:27 +0300

    Make tests for user parameter validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with valid" do
+    describe "email and password" do
+      let(:valid_user) { FactoryGirl.create(:user) }
+      it "should be saved" do
+        expect(valid_user).to be_valid
+      end
+    end
+  end
+  describe "with invalid" do
+    describe "email and password" do
+      let(:invalid_user) { User.new(email: "asd.com", password: "asd", password_confirmation: "asd") }
+      it "should not be saved" do
+        expect(invalid_user).not_to be_valid
+      end
+    end
+    describe "email" do
+      let(:user) { User.new(email: "asd/dfkfk.com", password: "Password1", password_confirmation: "Password1") }
+      it "should not be saved" do
+        expect(user).not_to be_valid
+      end
+    end
+    describe "password" do
+      let(:user) { User.new(email: "user@example.com", password: "asswo", password_confirmation: "asswo") }
+      it "should not be saved" do
+        expect(user).not_to be_valid
+      end
+    end
+    describe "password confirmation" do
+      let(:user) { User.new(email: "user@example.com", password: "Password1", password_confirmation: "Password2") }
+      it "should not be saved" do
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
