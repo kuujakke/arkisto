27dbfc55769d8f2f9e439cd920ec7ec45c5efea9
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2017-04-05 17:52:45 +0300

    Assign @pictures and expect 'rendered' to have pictures

--- spec/views/pictures/index.html.haml_spec.rb
+++ spec/views/pictures/index.html.haml_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe "pictures/index.html.haml", type: :view do
   describe "with many pictures" do
     before :each do
-      assign(:picture_jpeg, FactoryGirl.create(:picture_jpeg))
-      assign(:picture_png, FactoryGirl.create(:picture_png))
-      assign(:picture_webp, FactoryGirl.create(:picture_webp))
-      assign(:picture_gif, FactoryGirl.create(:picture_gif))
+      assign(:pictures, [
+          FactoryGirl.create(:picture_jpeg),
+          FactoryGirl.create(:picture_png),
+          FactoryGirl.create(:picture_webp),
+          FactoryGirl.create(:picture_gif)])
     end
     it "should display many pictures" do
       render
-      expect(page).to have_css('img', :count => 4)
+      expect(rendered).to have_css('img', :count => 4)
     end
   end
 end
